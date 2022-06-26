3848875f845248308abb985c8c7b2e444d17e264
Comp <buca-rock@mail.ru>
2022-06-26 10:27:46 +0300

    добавили файл переменные и рандом

---
+++ peremen.py
@@ -0,0 +1,9 @@
+a = 8
+b = 10
+c = 'hello world'
+
+print(a)
+print(b)
+print(c)
+print(a+b)
+
---
+++ rand.py
@@ -0,0 +1,17 @@
+import random
+
+str_='hello world, hello Evgeny'
+f = 'ABCDEFJ'
+a = random.random()
+b = random.randint(1,50)
+d = random.choice(str_)
+c = random.uniform(1,10)
+e = random.choice(f)
+
+print('0-1(float)=',a)
+print('1-50(int)=',b)
+print('1-10(float)=',c)
+print(d)
+print(e)
+
+
